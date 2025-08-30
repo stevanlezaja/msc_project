8ef92fe39a1da4e5a0dd800ce83538e5ba2cba9b
stevan <stevan.lezaja@gmail.com>
2025-08-30 22:50:34 +0200

    tracking time in run()

--- runner/run.py
+++ runner/run.py
@@ -1,4 +1,5 @@
 from typing import Callable
+from time import sleep, time
 
 from fibers import Fiber
 from raman_amplifier import RamanAmplifier
@@ -9,9 +10,14 @@ class Runner:
         self.fiber: Fiber = None
         self.raman_amplifier: RamanAmplifier = None
         self.experiment: Callable = None
+        self.running = True
+        self.command_buffer: list[Callable] = []
+        self.start_time = time()
 
     def run(self):
-        pass
+        while self.running:
+            print(f"Running for {time() - self.start_time}")
+            sleep(0.1)
 
     def set_raman_amplifier(self):
         pass
