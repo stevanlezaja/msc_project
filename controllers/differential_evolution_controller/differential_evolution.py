bb4509c88083388d86ea4e5eba8c5846c1df2d75
stevan <stevan.lezaja@gmail.com>
2025-11-04 17:08:59 +0100

    control normalization

--- controllers/differential_evolution_controller/differential_evolution.py
+++ controllers/differential_evolution_controller/differential_evolution.py
@@ -5,6 +5,7 @@ import raman_amplifier as ra
 
 from ..controller_base import Controller
 
+
 class DifferentialEvolutionController(Controller):
     def __init__(self):
         super().__init__()
@@ -12,8 +13,8 @@ class DifferentialEvolutionController(Controller):
     def get_control(self, curr_input: ra.RamanInputs, curr_output: ra.Spectrum[ct.Power], target_output: ra.Spectrum[ct.Power]) -> ra.RamanInputs:
         def optimization_fn(curr_input: ra.RamanInputs) -> float:
             return ra.mse(curr_output, target_output)
-        result = scipy.optimize.differential_evolution(optimization_fn, bounds=[[0.25, 0.75], [1420, 1490]], maxiter=1)
-        return ra.RamanInputs(powers=[ct.Power(float(result.x[0]), 'W')], wavelengths=[ct.Length(float(result.x[1]), 'nm')])
+        result = scipy.optimize.differential_evolution(optimization_fn, bounds=[[0.25, 0.75], [1420, 1490]], maxiter=1000)
+        return ra.RamanInputs(powers=[ct.Power(float(result.x[0]), 'W')], wavelengths=[ct.Length(float(result.x[1]), 'nm')]) - curr_input
 
     def update_controller(self, error: ra.Spectrum[ct.Power], control_delta: ra.RamanInputs) -> None:
         return
