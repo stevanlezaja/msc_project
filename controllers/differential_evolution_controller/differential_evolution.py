6e5841327c376ad17ce447b4f7e3b504cdcacf1a
stevan <stevan.lezaja@gmail.com>
2025-11-04 14:58:56 +0100

    using scipy function to get control

--- controllers/differential_evolution_controller/differential_evolution.py
+++ controllers/differential_evolution_controller/differential_evolution.py
@@ -6,8 +6,11 @@ class DifferentialEvolutionController(Controller):
     def __init__(self):
         super().__init__()
 
-    def get_control(self, curr_input: RamanInputs, curr_output: Spectrum[Power], target_output: Spectrum[Power]) -> RamanInputs:
-        return RamanInputs()
+    def get_control(self, curr_input: ra.RamanInputs, curr_output: ra.Spectrum[ct.Power], target_output: ra.Spectrum[ct.Power]) -> ra.RamanInputs:
+        def optimization_fn(curr_input: ra.RamanInputs) -> float:
+            return ra.mse(curr_output, target_output)
+        result = scipy.optimize.differential_evolution(optimization_fn, bounds=[[0.25, 0.75], [1420, 1490]], maxiter=1)
+        return ra.RamanInputs(powers=[ct.Power(float(result.x[0]), 'W')], wavelengths=[ct.Length(float(result.x[1]), 'nm')])
 
-    def update_controller(self, error: Spectrum[Power], control_delta: RamanInputs) -> None:
+    def update_controller(self, error: ra.Spectrum[ct.Power], control_delta: ra.RamanInputs) -> None:
         return
